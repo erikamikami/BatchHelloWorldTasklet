be38b9dfed76fa2229c7b9b36ee09f5e93993657
erikamikami <g.erika3up@gmail.com>
2022-08-30 09:16:54 +0900

    Tasklet作成

--- src/main/java/com/example/config/BatchConfig.java
+++ src/main/java/com/example/config/BatchConfig.java
@@ -1,5 +1,45 @@
 package com.example.config;
 
-public class BatchConfig {
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableBatchProcessing // SpringBatchの設定クラスであること明示
+public class BatchConfig {
+	
+	// jobを生成する
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+	
+	// stepを生成する
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+	
+	@Autowired
+	private Tasklet helloTasklet;
+	
+	// stepを生成する
+	@Bean
+	public Step taskletStep1() {
+		return stepBuilderFactory.get("HelloTaskletStep1") // 引数にstepの名前を設定。これがDBに登録され、いつどのstepが実行されたのか確認できるようになる
+								.tasklet(helloTasklet) // stepをtaskletに設定する。Taskletインターフェイスを実装したクラスを引数に。
+								.build();
+	}
+	
+	@Bean
+	public Job taskletJob() {
+		return jobBuilderFactory.get("HelloWorldTaskletJob") // 引数にjobの名前を設定。これがDBに登録され、いつどのjobが実行されたのか確認できるようになる
+								.incrementer(new RunIdIncrementer()) // jobのIDをインクリメントさせるクラスを指定。主キーとなるjobIDを重複させないためにインクリメンターが必要
+								.start(taskletStep1()) // 最初に実行するStepを指定する
+								.build();
+	}
+	
 }
--- src/main/java/com/example/tasklet/HelloTasklet.java
+++ src/main/java/com/example/tasklet/HelloTasklet.java
@@ -1,5 +1,24 @@
 package com.example.tasklet;
 
-public class HelloTasklet {
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@Component // TaskletとChunkは、Bean登録が必要
+@StepScope // Scopeをつけないと、スコープがシングルトンになる。Stepの開始から終了までインスタンスが存在し、複数のStepを実行したり、並列処理が可能になる。TascletとChunkにはStepScopeをつけよう
+@Slf4j
+public class HelloTasklet implements Tasklet{ // Taskletインターフェイスを実装すれば、Tascletの処理が作れる
+
+	@Override
+	public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+		log.info("Hello, World");
+		return RepeatStatus.FINISHED;
+	}
 
 }
