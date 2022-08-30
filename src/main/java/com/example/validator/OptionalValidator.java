d7b9ad2ca72d90799d83efebbc04f64ce2177e9e
erikamikami <g.erika3up@gmail.com>
2022-08-30 15:55:01 +0900

    プロパティファイル追加

--- src/main/java/com/example/validator/OptionalValidator.java
+++ src/main/java/com/example/validator/OptionalValidator.java
@@ -4,14 +4,21 @@ import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.JobParametersValidator;
 
-public class OptionalValidator implements JobParametersValidator{
+import io.micrometer.core.instrument.util.StringUtils;
+
+public class OptionalValidator implements JobParametersValidator {
 
 	@Override
 	public void validate(JobParameters parameters) throws JobParametersInvalidException {
 		// パラメータを取得
 		String key = "option1";
 		String option1 = parameters.getString(key);
-		
+
+		// 存在チェック
+		if (StringUtils.isEmpty(option1)) {
+			return;
+		}
+
 		// 数値型かどうかチェック
 		try {
 			Integer.parseInt(option1);
@@ -19,7 +26,7 @@ public class OptionalValidator implements JobParametersValidator{
 			String errorMessage = "Not Number:value=" + option1;
 			throw new JobParametersInvalidException(errorMessage);
 		}
-		
+
 	}
 
 }
